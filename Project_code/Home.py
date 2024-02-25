8d3faf4e1612fa11f723dc350b43911cd57158a7
Sebastián Carmona Ángel <68665717+Sebasc322@users.noreply.github.com>
2024-02-25 18:05:35 -0500

    Add files via upload

---
+++ Project_code/Home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+st.set_page_config(page_title="Home", page_icon=":house:")
+
+st.title("Cover Letter creation tool")
+
+st.write("""This tool is designed to help you create a cover letter for a job application based on your Resume.""")
+           
+         
+st.write("""Have you ever wondered if a job posting you found at Linkedin or any other job posting site is real or fake? And if it is real, how do you create a cover letter that will help you stand out from the crowd?""")
+st.write("""Our tool not only will help you create a perfect cover letter based on your Resume and the job posting, but also will help you verify if the job posting is real or fake.
+         """)
+
+st.image('cover.jpg', caption='Cover Letter',use_column_width=True)
